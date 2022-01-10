886d1a59781c591394b4df978b75fead12cad9d6
Brad Johnson <2021bradley@gmail.com>
2022-01-10 16:39:44 -0600

    Completed substrings project

---
+++ substrings.rb
@@ -0,0 +1,16 @@
+def substrings(text, dictionary)
+  
+  lower_text = text.downcase
+  hash = Hash.new(0)
+
+  dictionary.each do |word|
+    matches = lower_text.scan(word).length
+    hash[word] = matches unless matches == 0
+  end
+
+  hash
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
